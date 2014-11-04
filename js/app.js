93d647593dba9ff3e24c1fa66b48631548cdae5a
jiazeng <sophbj93@uw.edu>
2014-11-04 12:10:16 -0800

    in class work

--- js/app.js
+++ js/app.js
@@ -5,3 +5,27 @@
 
 "use strict";
 
+angular.module('AngularDemo', [])
+    .controller('HelloController', function($scope) { //$scope will be availabe in any views
+        $scope.person = {
+            firstName: null,
+            lastName: null
+        };
+    })
+    .controller('MoviesController', function($scope) {
+        $scope.movies = movies;
+        $scope.sortCol ='rank';
+        $scope.sortBy = function(sortCol) {
+            if ($scope.sortCol == sortCol) {
+                $scope.sortReverse = !$scope.sortReverse;
+            }
+            else {
+                $scope.sortCol = sortCol;
+                $scope.sortReverse = false;
+            }
+        };
+        $scope.isSortedBy = function(colName) {
+            return colName == $scope.sortCol;
+        }
+    });
+
